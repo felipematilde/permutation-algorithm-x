d472e546a5fb61c3f7316ee8aee4c15915368438
FMATILDE <felipe.matilde@embraer.com.br>
2025-04-02 22:47:09 -0300

    Add statistics evaluators

--- main.c
+++ main.c
@@ -5,11 +5,29 @@ void algorithmX(int n);
 void populateWithInt(int*vector, int n);
 bool testK(int* L, int k);
 void printA(char*A, int n);
-int count = 1;
+int permutationsCount = 0;
+int comparisonsCount = 0;
+int attributionsCount = 0;
 
 void main() {
-	int n = 10;
-	algorithmX(n);
+	FILE *file;
+	file = fopen("result.csv", "w");
+	fprintf(file, "Size,Permutations,Comparisons,Attributions,\n");
+	for (int n = 1; n <= 10; n++) {
+		permutationsCount = 0;
+		comparisonsCount = 0; 
+		attributionsCount = 0;
+		algorithmX(n);
+		fprintf(file, "%d,%d,%d,%d,\n", n, permutationsCount, comparisonsCount, attributionsCount);
+	}	
+	
+	//printf("\n");
+	//printf("Permutations = %d\n", permutationsCount);
+	//printf("Attributions = %d\n", attributionsCount);
+	//printf("Comparisons = %d\n", comparisonsCount);
+
+	fclose(file);
+
 }
 
 void algorithmX(int n) {
@@ -18,54 +36,56 @@ void algorithmX(int n) {
 	char A[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','W','Y','Z'};
 	int k, p, q;
 
-	//X1
 X1:
 	populateWithInt(L, n);
 	L[n] = 0;
 	k = 1;
+	attributionsCount +=2;
 
-	//X2
 X2:
 	p = 0;
 	q = L[0];
+	attributionsCount += 2;
 	
-	//X3
 X3:
 	A[k] = q+64;
+	attributionsCount++;
+	comparisonsCount++;
 	if (!testK(A, k)) {
 		goto X5;
 	}
+	comparisonsCount++;
 	if (k == n) {
-		//Visit A
-		printA(A, n); //????
+		printA(A, n);
 		goto X6;
 	}
 
-	//X4
-X4://
+X4:
 	U[k] = p;
 	L[p] = L[q];
 	k++;
+	attributionsCount += 3;
 	goto X2;
 
-	//X5
 X5:
-	//A[k] ++;
 	p = q;
 	q = L[p];
+	attributionsCount += 2;
+	comparisonsCount++;
 	if (q != 0) {
 		goto X3;
 	}
-
-	//X6
 X6:
 	k--;
+	attributionsCount++;
+	comparisonsCount++;
 	if (k == 0) {
 		goto XEND;
 	}
 	p = U[k];
 	q = A[k]-64;
 	L[p] = q;
+	attributionsCount += 3;
 	goto X5;
 
 XEND:
@@ -85,10 +105,12 @@ bool testK(int* L, int k) {
 }
 
 void printA(char*A, int n) {
+	permutationsCount++;
+	attributionsCount += n;
+	comparisonsCount += n+1;
 	for (int i = 1; i <= n; i++) {
 		printf("%c", A[i]);
 	}
-	printf(" - %d", count);
+	printf(" - %d", permutationsCount);
 	printf("\n");
-	count++;
 }
